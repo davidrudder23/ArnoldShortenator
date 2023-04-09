d8fa303ead386395a7126a86693f3bfe2d4f6974
Dave Rudder <drig@noses.org>
2023-04-09 09:51:07 -0600

    Deleting works

---
+++ src/main/resources/templates/crud.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#add-button").click(function() {
+        var slug = $("#slug").val();
+        var destination = $("#destination").val();
+
+        if (slug.length < 3) {
+            return;
+        }
+
+        if (destination.length < 3) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "/api",
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "slug": $("#slug").val(),
+                "destinationURL": $("#destination").val()
+            }),
+            success: function() {
+            getMatches($("#slug").val())
+            }
+        });
+    })
+});
+
+function deleteMapping(slug) {
+console.log("deleting "+slug)
+     $.ajax({
+            type: "DELETE",
+            url: "/api/"+slug,
+            success: function() {
+                getMatches($("#slug").val());
+            }
+        });
+}
